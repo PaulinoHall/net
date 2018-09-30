8e581941e365e6f16c475892ecba362b85a8c0e7
PaulinoHall <paolagalvezmedina@gmail.com>
2018-09-30 15:00:33 -0300

    Add files via upload

---
+++ ContraSuVoluntad/ContraSuVoluntad/Controlador/DAOCliente.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.Data.OracleClient; //Primer intento oracle
+using Modelo;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Controlador
+{
+    public class DAOCliente
+    {
+        //variable oracle
+
+        //Nombre base de datos: funeraria, aun no creada en sql developer
+
+        private OracleConnection conn;
+
+        public DAOCliente()
+        {
+            conn = new Conexion().obtenerConexion(); //inicia conexion
+        }
+
+     
+
+        public bool guardar(Cliente cli)
+        {
+            try
+            {
+                string orcl = string.Format("insert into funeraria values ('{0}','{1}','{2}','{3}', '{4}','{5}', '{6}','{7}')",
+                                             cli.Rut, cli.Nombre, cli.Telefono, cli.Direccion, cli.Edad, cli.TipoContrato, cli.CantidadCuotas, cli.Adicionales);
+
+                OracleCommand cmd = new OracleCommand(orcl, conn); //Prepara consulta, aparece obsoleto*
+
+                conn.Open();
+                // abre conexion
+
+                int cantidadFilas = cmd.ExecuteNonQuery(); //ejecuta consulta
+
+                conn.Close();
+
+                return cantidadFilas > 0; //retorna true si inserto.
+
+            }
+            catch (Exception ex)
+            {
+                conn.Close();
+                return false;
+            }
+        }
+
+        public List<Cliente> listar()
+        {
+            try
+            {
+                List<Cliente> lista = new List<Cliente>();
+                string orcl = "select * from funeraria";
+
+                OracleCommand cmd = new OracleCommand(orcl, conn);
+
+                conn.Open();
+
+                OracleDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    Cliente cli = new Cliente();
+
+                    cli.Rut = Convert.ToString(reader["rut"]);
+                    cli.Nombre = Convert.ToString(reader["nombre"]);
+                    cli.Telefono = Convert.ToInt32(reader["telefono"]);
+                    cli.Direccion = Convert.ToString(reader["direccion"]);
+                    cli.Edad = Convert.ToInt32(reader["edad"]);
+                    //cli.TipoContrato = Convert.ToString(reader["tipocontrato"]); //Falta convertir a tipo contrato
+                    cli.CantidadCuotas = Convert.ToInt32(reader["cantidadcuotas"]);
+                    cli.Adicionales = Convert.ToString(reader["adicionales"]);
+
+                    lista.Add(cli); //agrega
+
+                }
+
+                conn.Close();
+
+                return lista;
+            }
+            catch (Exception ex )
+            {
+
+                conn.Close();
+                return null;
+            }
+        }
+
+        public Cliente buscar(string rut)
+        {
+            try
+            {
+                Cliente cli = new Cliente(); //cliente en blanco
+
+                string orcl = string.Format("select * from funeraria where rut = {0}",rut);
+
+                OracleCommand cmd = new OracleCommand(orcl, conn);
+
+                conn.Open();
+
+                OracleDataReader reader = cmd.ExecuteReader();
+
+
+                while (reader.Read())
+                {
+                    Cliente cli = new Cliente();
+
+                    cli.Rut = Convert.ToString(reader["rut"]);
+                    cli.Nombre = Convert.ToString(reader["nombre"]);
+                    cli.Telefono = Convert.ToInt32(reader["telefono"]);
+                    cli.Direccion = Convert.ToString(reader["direccion"]);
+                    cli.Edad = Convert.ToInt32(reader["edad"]);
+                    //cli.TipoContrato = Convert.ToString(reader["tipocontrato"]); //Falta convertir a tipo contrato
+                    cli.CantidadCuotas = Convert.ToInt32(reader["cantidadcuotas"]);
+                    cli.Adicionales = Convert.ToString(reader["adicionales"]);
+
+                }
+
+                conn.Close();
+                return cli; //retorna el cliente
+
+
+            }
+            catch (Exception ex)
+            {
+                conn.Close();
+                return null;
+            }
+        }
+
+        public bool eliminar(string rut)
+        {
+            try
+            {
+                string orcl = string.Format("delete from funeraria where rut = {0}",rut);
+
+                OracleCommand cmd = new OracleCommand(orcl, conn);
+
+                conn.Open();
+
+                int cantidadFilas = cmd.ExecuteNonQuery();
+
+                conn.Close();
+
+                return cantidadFilas > 0;
+
+            }
+            catch (Exception ex)
+            {
+                conn.Close();
+                return false;
+            }
+        }
+
+        public bool modificar(Cliente cli)
+        {
+            try
+            {
+                string orcl = string.Format("insert into funeraria values ('{0}','{1}','{2}','{3}', '{4}','{5}', '{6}','{7}')",
+                                             cli.Rut, cli.Nombre, cli.Telefono, cli.Direccion, cli.Edad, cli.TipoContrato, cli.CantidadCuotas, cli.Adicionales);
+
+                OracleCommand cmd = new OracleCommand(orcl, conn); //Prepara consulta, aparece obsoleto*
+
+                conn.Open();
+                // abre conexion
+
+                int cantidadFilas = cmd.ExecuteNonQuery(); //ejecuta consulta
+
+                conn.Close();
+
+                return cantidadFilas > 0; //retorna true si inserto.
+
+            }
+            catch (Exception)
+            {
+                conn.Close();
+                return false;
+            }
+        }
+
+    }
+}
---
+++ ContraSuVoluntad/ContraSuVoluntad/Modelo/Cliente.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo
+{
+    public enum TipoContrato { Silver = 0, Platino = 1, Gold = 2 }
+    //Silver : 300.000 anual
+    //Gold 1.000.000
+    //Platino: 500.000
+
+    public class Cliente
+    {
+        private string _rut;
+        private string _nombre;
+        private int _telefono;
+        private string _direccion;
+        private int _edad;
+        private TipoContrato _tipoContrato;
+        private int _cantidadCuotas;
+        private string _adicionales;
+
+        public string Rut
+        {
+            get
+            {
+                return _rut;
+            }
+
+            set
+            {
+                _rut = value;
+            }
+        }
+
+        public string Nombre
+        {
+            get
+            {
+                return _nombre;
+            }
+
+            set
+            {
+                _nombre = value;
+            }
+        }
+
+        public int Telefono
+        {
+            get
+            {
+                return _telefono;
+            }
+
+            set
+            {
+                _telefono = value;
+            }
+        }
+
+        public string Direccion
+        {
+            get
+            {
+                return _direccion;
+            }
+
+            set
+            {
+                _direccion = value;
+            }
+        }
+
+        public int Edad
+        {
+            get
+            {
+                return _edad;
+            }
+
+            set
+            {
+                if (value >= 18)
+                {
+                    _edad = value;
+                }
+                else
+                {
+                    throw new Exception("Debe ser mayor de edad.");
+                }
+            }
+        }
+
+        public TipoContrato TipoContrato
+        {
+            get
+            {
+                return _tipoContrato;
+            }
+
+            set
+            {
+                _tipoContrato = value;
+            }
+        }
+
+        public int CantidadCuotas
+        {
+            get
+            {
+                return _cantidadCuotas;
+            }
+
+            set
+            {
+                if (value >= 3 && value <= 12)
+                {
+                    _cantidadCuotas = value;
+                }
+                else
+                {
+                    throw new Exception("Cantidad de cuotas fuera de rango.");
+                }
+            }
+        }
+
+        public string Adicionales
+        {
+            get
+            {
+                return _adicionales;
+            }
+
+            set
+            {
+                _adicionales = value;
+            }
+        }
+    }
+}
